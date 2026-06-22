d4927f6bdad3fbc427059802fa1cd1bba0a3c8b2
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-06-22 17:58:49 +0000

    Overdue commit

---
+++ itinerary_watch.py
@@ -0,0 +1,118 @@
+import json
+import logging
+import typing
+import urllib.parse
+import uuid
+
+import aws_lambda_powertools.utilities.data_classes
+import aws_lambda_powertools.utilities.typing
+
+
+_logger: logging.Logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
+
+
+def sns_message_entry_point(event: aws_lambda_powertools.utilities.data_classes.SNSEvent,
+                            _context: aws_lambda_powertools.utilities.typing.LambdaContext | None) -> None:
+
+    processed_sns_message_ids: list[uuid.UUID] = []
+
+    for curr_event_record in event.records:
+        curr_sns_message_id: uuid.UUID  = uuid.UUID(curr_event_record.sns.message_id)
+        _logger.info(f"Starting to process new SNS message with ID {str(curr_sns_message_id)}")
+
+        try:
+            parsed_payload: dict[str, int | str] = json.loads(curr_event_record.sns.message)
+        except Exception as e:
+            _logger.warning(f"Could not parse JSON from SNS message payload, error: {e}")
+            continue
+
+        _logger.debug(f"Got JSON payload: {json.dumps(parsed_payload, indent=4, sort_keys=True)}")
+
+        # Make sure it's a valid message
+        if not _valid_json(parsed_payload):
+            _logger.warning(f"Got invalid JSON, aborting: {json.dumps(parsed_payload, indent=4, sort_keys=True)}")
+            continue
+
+        valid_search_url: str = typing.cast(str, parsed_payload["search_url"])
+
+        _logger.info(f"SNS message ID {str(curr_sns_message_id)} contains search URL: \"{valid_search_url}\"")
+
+
+def _valid_json(parsed_payload: dict[str, int | str]) -> bool:
+
+    # Needs to be a dict with proper keys and values
+
+    if not isinstance(parsed_payload, dict):
+        _logger.warning("Parsed JSON was not a dict")
+        return False
+
+    if not len(parsed_payload) == 2:
+        _logger.warning("Dict did not have exactly two keys")
+        return False
+
+    check_keys: list[str] = ["schema_version", "search_url"]
+    if not all(key in parsed_payload for key in check_keys):
+        _logger.warning("Dict did not contain all of the expected keys")
+        return False
+
+    if not isinstance(parsed_payload["schema_version"], int):
+        _logger.warning("Schema version value not an integer")
+        return False
+
+    supported_schema_versions: set[int] = {
+        1,
+    }
+    if parsed_payload["schema_version"] not in supported_schema_versions:
+        _logger.warning("Unsupported schema version")
+        return False
+
+    if not isinstance(parsed_payload["search_url"], str):
+        _logger.warning("search_url value not a string")
+        return False
+
+    if not _valid_url(typing.cast(str, parsed_payload["search_url"])):
+        _logger.warning("search_url value not a valid URL")
+        return False
+
+    return True
+
+
+def _valid_url(possible_url: str) -> bool:
+    try:
+        result = urllib.parse.urlparse(possible_url)
+        # Ensure it has a scheme (e.g., http/https) and a domain name
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
+if __name__ == "__main__":
+
+    def _create_fake_sns_event() -> aws_lambda_powertools.utilities.data_classes.SNSEvent:
+        return aws_lambda_powertools.utilities.data_classes.SNSEvent(
+            {
+                "Records": [
+                    {
+                        "EventVersion": "1.0",
+                        "EventSubscriptionArn": "arn:aws:sns:us-east-1:123456789012:ExampleTopic:xxxx",
+                        "EventSource": "aws:sns",
+                        "Sns": {
+                            "SignatureVersion": "1",
+                            "Signature": "EXAMPLE",
+                            "SigningCertUrl": "EXAMPLE",
+                            "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+                            "Message": "{\"schema_version\": 1, " + \
+                                "\"search_url\": \"https://www.celebritycruises.com/cruises?search=nights:9~11,gte12|startDate:2028-01-01~2028-01-31|visiting:CARI&sort=by:NIGHTS|order:DESC&country=USA&currency=USD\"}",
+                            "MessageAttributes": {},
+                            "Timestamp": "1970-01-01T00:00:00.000Z",
+                            "TopicArn": "arn:aws:sns:us-east-1:123456789012:ExampleTopic",
+                            "UnsubscribeUrl": "EXAMPLE"
+                        }
+                    }
+                ]
+            }
+        )
+
+    logging.basicConfig()
+    sns_message_entry_point(_create_fake_sns_event(), None)
