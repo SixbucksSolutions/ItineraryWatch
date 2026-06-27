b26fda2e93adc652d556d991d3d2d3095553e1dd
Terry Ott <Terry.Ott@gmail.com>
2026-06-27 16:11:55 -0400

    bad final column

--- db/itinerary-watch-schema.sql
+++ db/itinerary-watch-schema.sql
@@ -3,6 +3,6 @@ DROP TABLE monitored_urls;
 CREATE TABLE monitored_urls (
     url_id UUID PRIMARY KEY DEFAULT uuidv7(),
     url VARCHAR,
-    url_last_scrape_timestamp TIMESTAMP WITH TIME ZONE,
+    url_last_scrape_timestamp TIMESTAMP WITH TIME ZONE
 );
 
